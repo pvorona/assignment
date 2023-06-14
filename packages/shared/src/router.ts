2ace65b8e285b837ca10648c21359ddeca22df74
pvorona <vorona.pavel.nickolaevich@gmail.com>
2023-06-14 10:10:06 +0300

    Add automatic address generation

--- packages/shared/src/router.ts
+++ packages/shared/src/router.ts
@@ -4,6 +4,16 @@ import { db } from './db';
 
 export const t = initTRPC.create();
 
+const customerIds = new Set<string>();
+
+setInterval(async () => {
+  for (const customerId of customerIds) {
+    const location = (Math.random() + 1).toString(36).substring(2);
+    const address = await db.address.create({ data: { customerId, location } });
+    console.log('Created address', JSON.stringify(address));
+  }
+}, 1_000);
+
 export const appRouter = t.router({
   getCustomers: t.procedure.query(() => {
     return db.customer.findMany();
@@ -11,15 +21,20 @@ export const appRouter = t.router({
   getCustomer: t.procedure.input(string()).query(({ input: id }) => {
     return db.customer.findFirst({ where: { id } });
   }),
-  deleteCustomer: t.procedure.input(string()).mutation(({ input: id }) => {
-    return Promise.all([
-      db.address.deleteMany({ where: { customerId: id } }),
-      db.customer.delete({ where: { id } }),
-    ]);
-  }),
-  createCustomer: t.procedure.input(string()).mutation(({ input: name }) => {
-    return db.customer.create({ data: { name } });
-  }),
+  deleteCustomer: t.procedure
+    .input(string())
+    .mutation(async ({ input: id }) => {
+      await db.address.deleteMany({ where: { customerId: id } });
+      await db.customer.delete({ where: { id } });
+      customerIds.delete(id);
+    }),
+  createCustomer: t.procedure
+    .input(string())
+    .mutation(async ({ input: name }) => {
+      const customer = await db.customer.create({ data: { name } });
+      customerIds.add(customer.id);
+      return customer;
+    }),
   updateCustomer: t.procedure
     .input(object({ id: string(), name: string().optional() }))
     .mutation(({ input: { id, ...payload } }) => {
